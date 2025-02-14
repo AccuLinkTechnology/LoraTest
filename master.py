ebf45fabae58fba080b844c39f93cbd5e12d82ea
Jay <jaymorse123@gmail.com>
2025-02-14 08:21:31 +0000

    added command

--- master.py
+++ master.py
@@ -1,8 +1,20 @@
 #Lora Testing 
+import time
 from serial import Serial
 
-usb_num = input("Enter USB Number. e.g ttyUSBX\n")
+# usb_num = input("Enter USB Number. e.g ttyUSBX\n")
 
-lora = Serial(port=f"/dev/ttyUSB{usb_num}")
+# lora = Serial(port=f"/dev/ttyUSB{usb_num}")
 
-lora.write("Checken")
+port = "/dev/ttyUSB2"
+baud_rate = 19600
+lora = Serial(port, baud_rate)
+# ===========   ===========   ===========   ===========   ===========
+
+command = "ER_CMD#B?"
+lora.write(command.encode() + b'\n')
+
+# ACK = "ACK"
+# lora.write(ACK.encode())
+
+print(lora.readline().decode('ascii').rstrip())
